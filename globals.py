f7585283eaadd89364880dae842bb1b4d654118e
Kartikey Sahu <37929060+kartikeysahu@users.noreply.github.com>
2018-04-03 20:16:20 +0530

    Added 8ball answers

--- globals.py
+++ globals.py
@@ -5,3 +5,25 @@ iuID = 281793428793196544
 welcomeName = 'welcome-good_bye'
 leaveName = 'welcome-good_bye'
 memesChannel = 'picture_memes'
+ballAnswers = [
+  "It is certain :8ball:",
+  "It is decidedly so :8ball:",
+  "Without a doubt :8ball:",
+  "Yes, definitely :8ball:",
+  "You may rely on it :8ball:",
+  "As I see it, yes :8ball:",
+  "Most likely :8ball:",
+  "Outlook good :8ball:",
+  "Yes :8ball:",
+  "Signs point to yes :8ball:",
+  "Reply hazy try again :8ball:",
+  "Ask again later :8ball:",
+  "Better not tell you now :8ball:",
+  "Cannot predict now :8ball:",
+  "Concentrate and ask again :8ball:",
+  "Don't count on it :8ball:",
+  "My reply is no :8ball:",
+  "My sources say no :8ball:",
+  "Outlook not so good :8ball:",
+  "Very doubtful :8ball:"
+]
