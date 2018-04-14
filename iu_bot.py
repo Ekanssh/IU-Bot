dba5df3004605c6e1c367573dedfecb541813622
yash <35102959+YashKandalkar@users.noreply.github.com>
2018-04-14 13:28:10 +0530

    Update iu_bot.py

--- iu_bot.py
+++ iu_bot.py
@@ -30,7 +30,7 @@ if creds.access_token_expired:
 
 db = client.open("IU DB").sheet1
 
-
+connected_to_db = False
 class aiopg_commands:
     def __init__(self):
         self.conn = None
@@ -42,6 +42,7 @@ class aiopg_commands:
                                    password='14e33018bf4991471bae5c11d2d57ab4424120299510a7891e61ee0123e81bc8',
                                    host='ec2-79-125-117-53.eu-west-1.compute.amazonaws.com')
         self.cursor = await self.conn.cursor()
+        connected_to_db = True
         
     async def execute(self, statement, args:tuple = None):
         if args is None:
@@ -275,6 +276,6 @@ async def dailiesCounter():
         
         
 
-
-bot.loop.create_task(dailiesCounter())
+if connected_to_db:
+    bot.loop.create_task(dailiesCounter())
 bot.run(globalvars.TOKEN)
