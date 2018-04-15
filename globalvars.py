213fb4b78b1c0558ea8cc9547463114c705e7a97
SVengat03 <33342399+SVengat03@users.noreply.github.com>
2018-04-15 17:20:02 +0800

    Update globalvars.py

--- globalvars.py
+++ globalvars.py
@@ -3,6 +3,7 @@ TOKEN = 'NDI5NjI1MTQyNDQ0OTQ5NTI0.DaEXMQ.jiRb0v8iOXnj8L3Vy78yAcNccMs'
 welcomeID = 429618676875001856
 rulesID = 429618590061297664
 iuID = 281793428793196544
+logID = 430752769146617866
 
 welcomeName = 'welcome-good_bye'
 leaveName = 'welcome-good_bye'
