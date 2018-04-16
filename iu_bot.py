d8dbc99dab81186a4398d78953faf49eff5ecd2c
SVengat03 <33342399+SVengat03@users.noreply.github.com>
2018-04-16 21:57:45 +0800

    Pegasus progress
    I added the feedback command.

--- iu_bot.py
+++ iu_bot.py
@@ -296,6 +296,13 @@ async def dailiesCounter():
             dailies_task.cancel()
             break
             
+class Miscellaneous:
+    @commands.command(aliases=['fb'])
+    async def feedback(self,ctx,*,message):
+        '''Please provide any feedback and report any bugs.'''
+        author=ctx.message.author.name+" said in "+"'"+ctx.guild.name+"'"
+        await bot.get_guild(381052278708240385).get_channel(435375286385770497).send(embed=discord.Embed(color=eval(hex(ctx.author.color.value)),title=author,description="#"+ctx.channel.name+":\n"+message))
+        await ctx.message.add_reaction('\u2705')
         
 class GracefulKiller:
   kill_now = False
