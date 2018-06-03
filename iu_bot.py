4e1dd1cb6d204f0c170775040b8f0dafc310761d
raniuday <34148863+raniuday@users.noreply.github.com>
2018-06-03 14:17:45 +0530

    1-> command for count past infinity-channel 2->fixed typo in welcome message

--- iu_bot.py
+++ iu_bot.py
@@ -7,7 +7,7 @@ import traceback, inspect
 import asyncio, discord, aiohttp
 import time, datetime
 import math, random
-import functools 
+import functools
 import textwrap,re
 import sqlite3
 import globalvars
@@ -18,8 +18,8 @@ import logging, signal
 from PIL import Image, ImageFont, ImageDraw
 from Paginator import Paginator
 from time import localtime, strftime
-from bs4 import BeautifulSoup as bs 
-import aiohttp 
+from bs4 import BeautifulSoup as bs
+import aiohttp
 from time import localtime, strftime
 import requests
 
@@ -45,20 +45,20 @@ class aiopg_commands:
 								   password='14e33018bf4991471bae5c11d2d57ab4424120299510a7891e61ee0123e81bc8',
 								   host='ec2-79-125-117-53.eu-west-1.compute.amazonaws.com')
 		self.cursor = await self.conn.cursor()
-		
+
 	async def execute(self, statement, args:tuple = None):
 		if args is None:
 			await self.cursor.execute(statement)
 		else:
 			await self.cursor.execute(statement, args)
-  
+
 aio = aiopg_commands()
 #custom functions for doing stuff goes here
 def tdm(td):
 	return ((td.days * 86400000) + (td.seconds * 1000)) + (td.microseconds / 1000)
 
-def calculate_level(level: 'current level') -> 'xp to reach next level': 
-	return (level**2+level)/2*100-(level*100) 
+def calculate_level(level: 'current level') -> 'xp to reach next level':
+	return (level**2+level)/2*100-(level*100)
 
 
 '''
@@ -81,9 +81,9 @@ async def on_ready():
 	await aio.connect()
 	await aio.execute("CREATE TABLE IF NOT EXISTS Dailies(id BIGINT, dailiesCount INT, remaining_timestamp TIMESTAMP)")
 	await aio.execute("CREATE TABLE IF NOT EXISTS profile(id BIGINT, reps INT, profile_background TEXT, badges TEXT, level INT, note TEXT, xp INT)")
-	
+
 	await bot.change_presence(status=discord.Status.dnd,activity=discord.Game(name="on Indians United [iu_help reveals commands]"))
-	
+
 	async def dbauth():
 		while True:
 			scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -95,12 +95,12 @@ async def on_ready():
 				client.login()
 			await asyncio.sleep(3000)
 	bot.loop.create_task(dbauth())
-	 
+
 @bot.event
 async def on_member_join(member):
 	if member.guild.id == 281793428793196544:
 		channel = bot.get_channel(429618676875001856)
-		await channel.send('Welcome to IU United, '+member.mention+'! Please enjoy your time here and hope you check #rules! :)')
+		await channel.send('Welcome to Indians United, '+member.mention+'! Please enjoy your time here and hope you check #rules! :)')
 
 @bot.event
 async def on_member_remove(member):
@@ -110,32 +110,40 @@ async def on_member_remove(member):
 
 @bot.event
 async def on_message(msg):
-	if msg.author.id==279318185524723712:
+	if msg.channel.id==434664516991844352: #counting channel
+ 		try:
+  			last_nos=[]
+  			async for i in ctx.channel.history(limit=2):
+   			last_nos.append(int(i.content))
+  			if last_nos[0]!=last_nos[1]+1:await message.delete()
+ 		except:
+  			await message.delete()
+	if msg.author.id==279318185524723712: # restricting Emp.ketan
 		content=msg.content.lower()
 		await msg.delete()
 		await msg.channel.send("**Emp.ketan said: **"+content)
-		
+
 	if msg.channel.name is globalvars.memesChannel:
 		for chr in list(string.ascii_letters):
 			if chr in str(msg.content):
 				await msg.delete()
-				
+
 	found = False
 	if not msg.author.bot:
 		await aio.execute("SELECT * FROM profile WHERE id = %s", (msg.author.id, ))
 		for i in await aio.cursor.fetchall():
 			if i is not None:
 				if i[0] == msg.author.id:
-				
+
 					found = True
-	
+
 					await aio.execute("SELECT * FROM profile WHERE id = %s", (msg.author.id, ))
 					xp = (await aio.cursor.fetchall())[0][6]
 					await aio.execute("UPDATE profile SET xp = %s WHERE id = %s", (xp + 5, msg.author.id, ))
-		
+
 					await aio.execute("SELECT * FROM profile WHERE id = %s", (msg.author.id, ))
 					level = (await aio.cursor.fetchall())[0][4]
-		
+
 					if xp == int(calculate_level(level)):
 						await aio.execute("UPDATE profile SET level = %s WHERE id = %s", (level + 1, msg.author.id, ))
 						await msg.channel.send("Congratulations, " + msg.author.mention + " you advanced to level {}".format(level + 1),delete_after=10)
@@ -149,13 +157,13 @@ async def on_message(msg):
 async def on_message_edit(before, after):
   await bot.process_commands(after)
 
-	
+
 class Admin:
 	'''For administrative purposes'''
-	
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
-	@commands.has_permissions(kick_members=True) 
+	@commands.has_permissions(kick_members=True)
 	async def kick(self, ctx, member: discord.Member):
 		'''Kick members from your server'''
 		try:
@@ -163,7 +171,7 @@ class Admin:
 			await ctx.message.add_reaction('\u2705')
 		except:
 			await ctx.message.add_reaction('\u274C')
-	
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	@commands.has_permissions(ban_members=True)
@@ -174,7 +182,7 @@ class Admin:
 			await ctx.message.add_reaction('\u2705')
 		except:
 			await ctx.message.add_reaction('\u274C')
-	
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command(aliases=['cr', 'updaterole'])
 	@commands.has_permissions(manage_roles=True)
@@ -189,37 +197,37 @@ class Admin:
 			await ctx.message.add_reaction('\u2705')
 		except:
 			await ctx.message.add_reaction('\u274C')
-					
+
 	@commands.command()
 	@commands.has_permissions(manage_messages=True)
 	async def purge(self, ctx, number: int):
 		'''Clears specified number of messages, ranging from 2 to 100'''
 		await ctx.channel.purge(limit=number)
-	
-	
+
+
 	@commands.command(hidden = True, name="sql")
 	async def _sql(self, ctx, statement, *args):
 		if not ctx.author.id in ownerid: return
-		try: 
+		try:
 			if len(args) > 0: await aio.execute(statement, *args)
 			else: await aio.execute(statement)
-		except Exception as e: 
+		except Exception as e:
 			await ctx.send(embed = discord.Embed(title="ERROR", description = "```" + str(e) + "```"))
 
 
 class General:
-	'''General commands'''  
+	'''General commands'''
 
 	@commands.command(aliases=['cricket', 'scores','cric','score'])
 	async def cricbuzz(self , ctx) :
 		url = "http://www.cricbuzz.com/"
 		data = []
-		async with aiohttp.ClientSession() as cs: 
+		async with aiohttp.ClientSession() as cs:
 			async with cs.get(url) as r:
 				html = await r.read()
 			soup = bs(html, 'html.parser')
 		t = soup.find_all(attrs={'class':'cb-ovr-flo'})
-		for u in t: 
+		for u in t:
 			if u is not None :
 				data.append(u.text)
 		tossres = data[5]
@@ -228,7 +236,7 @@ class General:
 		curr = data[0]
 		em = discord.Embed(title = "{0} vs {1}".format(tm[0],tm[1]) , description = "{5} ... \nCurrent Inning : {0} \n{1} : {2}\n{3} : {4}".format(curr,tm[0],inn[0],tm[1],inn[1],tossres)+"\n*Score Updated at* "+str(strftime("%a, %d %H:%M:%S", localtime())), colour = int(hex(random.randint(0,16777215)),16))
 		await ctx.send(embed = em)
-	'''	
+	'''
 	@commands.command(aliases = ['scoretable','ipltable','ipl_table','points','points_table','pt'])
 	async def pointstable(self, ctx) :
 		"""Displays the IPL Points Table """
@@ -254,7 +262,7 @@ class General:
 		em = discord.Embed(title = "Points Table IPL 2018" , description = table , colour = int(hex(random.randint(0,16777215)),16) )
 		await ctx.send(embed = em )
 		'''
-	
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	async def profile(self, ctx, mem: discord.Member = None):
@@ -263,7 +271,7 @@ class General:
 		mem = mem or ctx.message.author
 		if mem.bot:
 			await ctx.send("Sorry, bots don't have a profile... ~~*until they overthrow humans*~~")
-			return 
+			return
 		await aio.execute("SELECT * FROM profile WHERE id = %s", (mem.id, ))
 		for i in await aio.cursor.fetchall():
 			if i is not None:
@@ -275,13 +283,13 @@ class General:
 					  currentDaily = int((await aio.cursor.fetchall())[0][1])
 				  else :
 					  currentDaily = 0
-	  
+
 				  await aio.execute("SELECT * FROM profile WHERE id = %s", (mem.id,))
 				  level = (await aio.cursor.fetchall())[0][4]
-	  
+
 				  await aio.execute("SELECT * FROM profile WHERE id = %s", (mem.id,))
 				  note = (await aio.cursor.fetchall())[0][5]
-	  
+
 				  back = Image.open("Images/background.png")
 				  background = Image.open("Images/milky-way.jpg")
 				  background = background.crop((0, 0, 500, 215))
@@ -291,7 +299,7 @@ class General:
 				  level_font = ImageFont.truetype("Fronts/Quicksand-Regular.otf", 25)
 				  credits_reps_font = ImageFont.truetype("Fronts/Quicksand-Regular.otf", 20)
 				  note_font = ImageFont.truetype("Fronts/Quicksand-Regular.otf", 20)
-	  
+
 				  d = ImageDraw.Draw(back)
 				  d.rectangle([0, 160, 110, 270], fill = (255, 255, 255))
 				  async with aiohttp.ClientSession() as cs:
@@ -300,7 +308,7 @@ class General:
 							  ava.write(await r.read())
 				  avatar = Image.open("TEMPava.png")
 				  avatar = avatar.resize((100, 100))
-				  back.paste(avatar, (5, 165))    
+				  back.paste(avatar, (5, 165))
 				  d.text(text = str(mem), xy = (125, 215), font = font)
 				  d.line([(113, 219), (113, 500)], fill = (50, 50, 50), width = 3) #line beside ava
 
@@ -325,25 +333,25 @@ class General:
 		if not found:
 			await aio.execute("INSERT INTO profile VALUES (%s, %s, %s, %s, %s, %s, %s)", (mem.id, 0, 'milky-way', 'None', 1, 'I am imperfectly perfect...', 0))
 			await ctx.invoke(bot.get_command("profile"), mem)
-	   
-		
+
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	async def note(self, ctx, option="show", *, newNote = None):
 		 '''Set your profile's note'''
 		 if ctx.message.author.bot:
-		   return 
+		   return
 		 if option == "show":
-			 await aio.execute("SELECT * FROM profile WHERE id = %s", (ctx.message.author.id,))                      
+			 await aio.execute("SELECT * FROM profile WHERE id = %s", (ctx.message.author.id,))
 			 note =  (await aio.cursor.fetchall())[0][5]
 			 await ctx.send("Your current note is:\n" + "```" + note + "```")
 		 elif option == "set" and newNote is not None:
 			 await aio.execute("UPDATE profile SET note = %s WHERE id = %s", (newNote, ctx.message.author.id, ))
 			 await ctx.send("Your current note is:\n" + "```" + newNote + "```")
 		 elif option == "reset":
-			 await aio.execute("UPDATE profile SET note = %s WHERE id = %s", ('I am imperfectly perfect...', ctx.message.author.id, ))  
+			 await aio.execute("UPDATE profile SET note = %s WHERE id = %s", ('I am imperfectly perfect...', ctx.message.author.id, ))
 			 await ctx.send("Your profile's note has been reset to:\n" + '```I am imperfectly perfect...```')
-	'''        
+	'''
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	async def top(self, ctx):
@@ -365,7 +373,7 @@ class General:
 
 		for i in range (10, rows_count, 10):
 			await aio.execute("SELECT id, xp FROM profile ORDER BY xp DESC LIMIT %s OFFSET %s", (i, i-10))
-			em = discord.Embed(title = "Scoreboard for " + ctx.guild.name, 
+			em = discord.Embed(title = "Scoreboard for " + ctx.guild.name,
 									color = 0x00FFFF,
 									description = '')
 			position = 0
@@ -382,17 +390,17 @@ class General:
 			em.set_footer(text="Page {0} of {1}".format(page_index, int(rows_count/10)))
 			page_index += 1
 			embed_list.append(em)
-			
-			
-		info_embed = discord.Embed(title = "Help Info", 
-								   description = "\u23EA:  Go to the first page\n\u25C0:  Go to the previous page\n\u23F9:  Stop the help command\n\u25B6:  Go to the next page\n\u23E9:  Go to the last page\n\U0001f522:  Asks for a page number\n\u2139:  Shows this info", 
-								   colour = 0x2279DE)  
+
+
+		info_embed = discord.Embed(title = "Help Info",
+								   description = "\u23EA:  Go to the first page\n\u25C0:  Go to the previous page\n\u23F9:  Stop the help command\n\u25B6:  Go to the next page\n\u23E9:  Go to the last page\n\U0001f522:  Asks for a page number\n\u2139:  Shows this info",
+								   colour = 0x2279DE)
 		embed_list.append(info_embed)
 		await msg.edit(embed = embed_list[0])
 		pa = Paginator(bot, msg, ctx.message.author, 0)
 		await pa.paginate(embed_list)
-	'''    
-		
+	'''
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	async def ping(self, ctx):
@@ -401,14 +409,14 @@ class General:
 		res = msg.created_at - ctx.message.created_at
 		res = tdm(res)
 		await msg.edit(content=ctx.author.mention+', Pong! :ping_pong: Took {} ms'.format(res))
-	
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	async def say(self, ctx, *, something: commands.clean_content='IU Bot Bot here!'):
 		'''The bot becomes your copycat'''
 		await ctx.send(something)
 		await ctx.message.delete()
-   
+
 	@commands.command()
 	#dont dare to touch this command
 	async def bday(self, ctx, bDay):
@@ -420,7 +428,7 @@ class General:
 			userID = str(ctx.message.author.id)
 			db.insert_row([userID, bDay], index=1, value_input_option='RAW')
 			await ctx.message.add_reaction('\u2705')
-	
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	async def now(self, ctx):
@@ -431,7 +439,7 @@ class General:
 		embed.add_field(name='Date', value='{}'.format(m[0]))
 		embed.add_field(name='Time', value='{}GMT'.format(m[1]))
 		await ctx.send(embed=embed)
-	
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command(name = '8ball')
 	async def _func(self, ctx, *, question = ' '):
@@ -439,7 +447,7 @@ class General:
 		if question[-1] == '?':
 			return await ctx.send(random.choice(globalvars.ballAnswers))
 		await ctx.send('`Try again with a question!`')
-	
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	async def choose(self, ctx, *, options):
@@ -447,7 +455,7 @@ class General:
 		if len(options.split('|')) >= 2:
 			return await ctx.send(embed=discord.Embed(title="And the bot has chosen...",description=random.choice(options.split('|')),color=discord.Color.gold()))
 		await ctx.send(embed=discord.Embed(title='You invoked command incorrectly!',description='Give at least two options separated by **|**',color=discord.Color.red()))
-	 
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	async def rps(self, ctx, value):
@@ -483,15 +491,15 @@ class Economy:
 		elif xp<10000:embed=discord.Embed(title=person.name+"'s level",description="Level "+str(level)+"\n"+str(xp)+" xp",colour=discord.Colour.gold())
 		else:embed=discord.Embed(title=person.name+"'s level",description="Level "+str(level)+"\n"+str(xp)+" xp",colour=discord.Colour.from_rgb(20, 30, 179))
 		await ctx.send(embed=embed)
-		
-		
+
+
 	@commands.cooldown(rate=1, per=15, type=commands.BucketType.user)
 	@commands.command(aliases=['daily'])
 	async def dailies(self, ctx):
             '''Get your free ₹200'''
             if ctx.message.author.bot:
                   await ctx.send("Sorry, bots have nothing to do with money")
-                  return 
+                  return
             msg_timestamp = ctx.message.created_at
             found = False
             await aio.execute("SELECT * FROM Dailies WHERE id= %s", (ctx.message.author.id, ))
@@ -502,34 +510,34 @@ class Economy:
                               found = True
                               await aio.execute("SELECT * FROM Dailies WHERE id= %s", (ctx.message.author.id, ))
                               previous_msg_timestamp = (await aio.cursor.fetchall())[0][2]
-                              
+
                               remaining_timestamp = previous_msg_timestamp - msg_timestamp
-			
+
                               await aio.execute("SELECT * FROM Dailies WHERE id= %s", (ctx.message.author.id, ))
                               currentDaily = int((await aio.cursor.fetchall())[0][1])
-                             
+
                               secondsRemaining = remaining_timestamp.seconds
                               time = str(datetime.timedelta(seconds = secondsRemaining)).split(":")
-                              
-                              if secondsRemaining >= 86400:                              
+
+                              if secondsRemaining >= 86400:
                                     currentDaily += 200
                                     await aio.execute("UPDATE Dailies SET dailiesCount = %s, remaining_timestamp = %s WHERE id = %s", (currentDaily, msg_timestamp, ctx.message.author.id, ))
                                     await ctx.send(":moneybag: | You got your 200 dialies!\n You have ₹{}".format(currentDaily))
-                        
+
                               else:
                                     await ctx.send("Sorry, you can claim your dailies in {0}hrs, {1}mins, {2}s\nYou have ₹{3}:moneybag:".format(time[0], time[1], time[2], currentDaily))
 
             if not found:
                   await aio.execute("INSERT INTO Dailies VALUES (%s, '200', %s)", (ctx.message.author.id, msg_timestamp))
                   await ctx.send(":moneybag: | You got your 200 dialies!\nYou have ₹200")
-			
+
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
 	@commands.command()
 	async def credits(self, ctx, otherMem: discord.Member = None):
 		'''Check your or someone else's credits'''
 		if ctx.message.author.bot or (otherMem is not None and otherMem.bot):
 			await ctx.send("Sorry, bot have nothing to do with money")
-			return 
+			return
 		found_in_db = False
 		if otherMem is None:
 			await aio.execute("SELECT * FROM Dailies WHERE id = %s", (str(ctx.message.author.id),))
@@ -537,7 +545,7 @@ class Economy:
 				if i is not None:
 					if i[0] == str(ctx.message.author.id):
 						found_in_db = True
-						await aio.execute("SELECT * FROM Dailies WHERE id = %s", (str(ctx.message.author.id),))                       
+						await aio.execute("SELECT * FROM Dailies WHERE id = %s", (str(ctx.message.author.id),))
 						await ctx.send(":moneybag: | You currently have ₹{0}".format((await aio.cursor.fetchall())[0][1]))
 			if not found_in_db:
 				await ctx.send(":moneybag: | You currently have ₹0")
@@ -547,11 +555,11 @@ class Economy:
 				if i is not None:
 					if i[0] == str(otherMem.id):
 						found_in_db = True
-						await aio.execute("SELECT * FROM Dailies WHERE id = %s", (str(otherMem.id),))                       
+						await aio.execute("SELECT * FROM Dailies WHERE id = %s", (str(otherMem.id),))
 						await ctx.send(":moneybag: | {0} currently has ₹{1}".format(otherMem.name, (await aio.cursor.fetchall())[0][1]))
 			if not found_in_db:
 				await ctx.send(":moneybag: | {0} currently has ₹0".format(otherMem.name))
-'''	
+'''
 async def dailiesCounter():
 	await bot.wait_until_ready()
 	await aio.connect()
@@ -565,8 +573,8 @@ async def dailiesCounter():
 				await aio.execute("UPDATE Dailies SET secToReset = %s WHERE id = %s", (str(tempTime), str(i[0]), ))
 		await asyncio.sleep(2)
 '''
-			
-			
+
+
 class Miscellaneous:
 	@commands.command(aliases=['fb'])
 	async def feedback(self,ctx,*,message):
@@ -574,6 +582,6 @@ class Miscellaneous:
 		author=ctx.message.author.name+" said in "+"'"+ctx.guild.name+"'"
 		await bot.get_guild(381052278708240385).get_channel(435375286385770497).send(embed=discord.Embed(color=eval(hex(ctx.author.color.value)),title=author,description="#"+ctx.channel.name+":\n"+message))
 		await ctx.message.add_reaction('\u2705')
-		
+
 
 bot.run(globalvars.TOKEN)
