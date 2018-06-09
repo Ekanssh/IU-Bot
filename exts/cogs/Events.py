f1f8dce9f0448b25acb55a8882f92168fcea23c6
Ekansh kashyap <cityexevision@gmail.com>
2018-06-09 15:39:35 +0530

    indention fix in events

--- exts/cogs/Events.py
+++ exts/cogs/Events.py
@@ -72,7 +72,7 @@ class Events:
 	    if not found:
 	        if not msg.author.bot:
 	            await self.bot.aio.execute("INSERT INTO profile VALUES (%s, %s, %s, %s, %s, %s, %s)", (msg.author.id, 0, 'milky-way', 'None', 1, 'I am imperfectly perfect...', 0))
-				await self.bot.process_commands(msg)
+		await self.bot.process_commands(msg)
 
 	async def on_message_edit(self, before, after):
 	    await self.bot.process_commands(after)
