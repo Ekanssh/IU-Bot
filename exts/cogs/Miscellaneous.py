0003ad9382d6a9b776f93757c151f94ece63bff5
SVengat03 <33342399+SVengat03@users.noreply.github.com>
2018-06-11 17:50:19 +0800

    Update Miscellaneous.py

--- exts/cogs/Miscellaneous.py
+++ exts/cogs/Miscellaneous.py
@@ -89,8 +89,8 @@ class Miscellaneous:
     @commands.cooldown(rate=1,per=8,type=commands.BucketType.guild)
     @commands.command()
     async def weather(self,ctx,*,location):
-        '''to get the weather'''
-        try:
+        #'''to get the weather'''
+        
             async with ctx.typing():
                 url="http://api.tanvis.xyz/weather/"+urllib.request.pathname2url(location)
                 f=await getjson(url)
@@ -103,8 +103,8 @@ class Miscellaneous:
                     embed.add_field(name="Wind Speed",value=f['windSpeed']);embed.set_thumbnail(url=f['icon'])
                     embed.set_footer(text="using **tanvis.xyz** API")
                     await ctx.send(embed=embed)
-        except:
-                await ctx.send("An error occurred. Please try again.",delete_after=3)
+        #except:
+                #await ctx.send("An error occurred. Please try again.",delete_after=3)
                 
     @commands.command()
     async def google(self, ctx, *, anything):
