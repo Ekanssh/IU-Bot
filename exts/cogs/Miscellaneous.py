f3e305305272c1758490f121105b14a69da72ac7
raniuday <34148863+raniuday@users.noreply.github.com>
2018-06-11 20:52:54 +0530

    mixed indentation fix

--- exts/cogs/Miscellaneous.py
+++ exts/cogs/Miscellaneous.py
@@ -18,21 +18,21 @@ class Miscellaneous:
         await self.bot.get_guild(381052278708240385).get_channel(435375286385770497).send(embed=discord.Embed(color=eval(hex(ctx.author.color.value)),title=author,description="#"+ctx.channel.name+":\n"+message))
         await ctx.message.add_reaction('\u2705')
 
-	@commands.command()
-	async def weather(self, ctx, *, location):
-		'''Gives you the weather of a location'''
-		async with aiohttp.ClientSession() as cs:
-			async with cs.get('http://api.tanvis.xyz/weather/{}'.format(location)) as res:
-				res = await res.json(encoding = 'utf-8')
-		em = discord.Embed(title = "Current weather of {}".format(location),
-							color = 0x00FFFF)
-		for i in res:
-			if i != 'error':
-				em.add_field(name = i, value= res[i])
-			else:
-				em.title = "Error"
-				em.description = "Could not find the location, {}".format(location)
-		await ctx.send(embed = em)
+    @commands.command()
+    async def weather(self, ctx, *, location):
+        '''Gives you the weather of a location'''
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('http://api.tanvis.xyz/weather/{}'.format(location)) as res:
+                res = await res.json(encoding = 'utf-8')
+        em = discord.Embed(title = "Current weather of {}".format(location),
+                            color = 0x00FFFF)
+        for i in res:
+            if i != 'error':
+                em.add_field(name = i, value= res[i])
+            else:
+                em.title = "Error"
+                em.description = "Could not find the location, {}".format(location)
+        await ctx.send(embed = em)
 
 
 
