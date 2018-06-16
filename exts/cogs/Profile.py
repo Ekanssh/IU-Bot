91df0f6be54616d22acc376da3974775afbe3f3c
yash <35102959+YashKandalkar@users.noreply.github.com>
2018-06-16 19:55:31 +0530

    Update Profile.py

--- exts/cogs/Profile.py
+++ exts/cogs/Profile.py
@@ -232,8 +232,8 @@ class Profile:
                                     return await ctx.send("You don't have enough money to buy a banner.")
                                 else:
                                     purchased_banners.append(item)
-                                    await self.bot.aio.execute("UPDATE profile SET banners_buyed = %s", (' '.join(purchased_banners)))
-                                    await self.bot.aio.execute("UPDATE Dailies SET dailiesCount = %s", (currentCredits - 1000, ))
+                                    await self.bot.aio.execute("UPDATE profile SET banners_buyed = %s WHERE id = %s", (' '.join(purchased_banners), ctx.author.id))
+                                    await self.bot.aio.execute("UPDATE Dailies SET dailiesCount = %s WHERE id = %s", (currentCredits - 1000, ctx.author.id))
                                     await ctx.send("You successfully buyed {}.\nSay `iu banner set <banner name> to set the respective banner.".format(item))
                             else:
                                 await ctx.send("Your purchase is cancelled.")
