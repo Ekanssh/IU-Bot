c009bbd6251e5b49357ff5e0cc23340b9fb2dbdc
raniuday <34148863+raniuday@users.noreply.github.com>
2018-11-29 15:05:31 +0530

    changed a hell lot in Admin.py

--- exts/cogs/Admin.py
+++ exts/cogs/Admin.py
@@ -14,29 +14,29 @@ class Admin:
         self.bot = bot
 
     @commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
-    @commands.command()
+    @commands.command(hidden=True)
     @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx, member: discord.Member):
+    async def kick(self, ctx, member: discord.Member,*,reason):
         '''Kick members from your server'''
         try:
-            await member.kick()
+            await member.kick(reason=reason)
             await ctx.message.add_reaction('\u2705')
         except:
             await ctx.message.add_reaction('\u274C')
 
     @commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
-    @commands.command()
+    @commands.command(hidden=True)
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, member: discord.Member):
+    async def ban(self, ctx, member: discord.Member,*,reason):
         '''Ban toxic members from your server'''
         try:
-            await member.ban()
+            await member.ban(reason=reason)
             await ctx.message.add_reaction('\u2705')
         except:
             await ctx.message.add_reaction('\u274C')
 
     @commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
-    @commands.command(aliases=['cr', 'updaterole'])
+    @commands.command(aliases=['cr', 'updaterole'],hidden=True)
     @commands.has_permissions(manage_roles=True)
     async def changerole(self, ctx, member: discord.Member, *, rolename):
         '''To add/remove a role from a person'''
@@ -50,25 +50,28 @@ class Admin:
         except:
             await ctx.message.add_reaction('\u274C')
 
-    @commands.command()
+    @commands.command(hidden=True)
+    @commands.has_permissions(manage_messages=True)
     async def warn(ctx, user:discord.Member, *, reason):
         '''warns an offender'''
         try:
             warn_embed=discord.Embed(title="You've been warned in Indians United",colour=discord.Colour.red())
-            if ctx.author.top_role.name in ["Queens","Lords","Nobles"]:
-                warn_embed.description="You've been warned by {} for the following reason: {}".format(ctx.author.name,reason)
-                await user.send(embed=warn_embed)
-            else:
-                warn_embed.description="You've been warned for trying to warn a user without the permissions."
-                await ctx.author.send(embed=warn_embed)
+            warn_embed.description=f"You've been warned for the following reason: {reason}")
+            await user.send(embed=warn_embed)
+            await ctx.send(f"Alright! {user.name} has been warned by {ctx.author.name} ")
         except Exception as e:
             await ctx.send(f"{e}")
 
-    @commands.command()
+    @commands.command(hidden=True)
     @commands.has_permissions(manage_messages=True)
     async def purge(self, ctx, number: int):
-        '''Clears specified number of messages, ranging from 2 to 100'''
-        await ctx.channel.purge(limit=number)
+        '''Clears specified number of messages'''
+        while number != 0:
+            if number<=99:
+                await ctx.channel.purge(limit=number+1)
+            else:
+                await ctx.channel.purge(limit=100)
+                number-=100
 
 
 def setup(bot):
