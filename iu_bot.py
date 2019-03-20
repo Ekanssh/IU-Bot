44a0151ce3925517c19ed157e6081f4ee938bb76
YashKandalkar <yashsk.yk@gmail.com>
2019-03-20 20:31:48 +0530

    fixed blacklist

--- iu_bot.py
+++ iu_bot.py
@@ -62,7 +62,7 @@ async def on_ready():
     bot.atlas_active_channels = {}
     bot.appid = "9c23ae0122eeb6cd076c3e2d726312ed"
     extentions = ("Admin", "Economy", "Events", "General",
-                  "repl", "Miscellaneous", "Profile","Roles")
+                  "repl", "Miscellaneous", "Profile","Roles", "Blacklist")
     for i in extentions:
         try:
             bot.load_extension("exts.cogs.{}".format(i))
