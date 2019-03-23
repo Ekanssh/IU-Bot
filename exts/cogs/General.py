9d9cac21339c56c233a059fccf29a4e861b42e2f
raniuday <34148863+raniuday@users.noreply.github.com>
2019-03-24 00:09:32 +0530

    Update General.py

--- exts/cogs/General.py
+++ exts/cogs/General.py
@@ -30,7 +30,7 @@ class General(commands.Cog):
         url = "http://www.cricbuzz.com/"
         data = []
         headers={
-            'user-agent': 'Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
             'cache-control' : 'no-cache'
         }
         async with aiohttp.ClientSession(headers=headers) as cs:
