ae5f9b7832a202739c2067413631ace2f753c389
Prometheus <33342399+SVengat03@users.noreply.github.com>
2019-06-16 13:04:20 +0800

    iu_bot.py: Added Prometheus to owner list too

--- iu_bot.py
+++ iu_bot.py
@@ -32,7 +32,8 @@ else:
 ownerid = {315728369369088003: "Ekansh",
            270898185961078785: "Shirious",
            388984732156690433: "Yash",
-           443961507051601931: "Uday"}
+           443961507051601931: "Uday",
+           586426079078514700: "Prometheus"}
 
 aio = aiopg_commands()  # used for database purposes
 
