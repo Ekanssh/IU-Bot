a1145bb9ff32bb46e46d2af2c9bbb719f9cc5ba3
Prometheus <33342399+SVengat03@users.noreply.github.com>
2019-06-16 13:14:15 +0800

    Paginated Help

--- exts/cogs/General.py
+++ exts/cogs/General.py
@@ -19,6 +19,7 @@ import aiohttp  # various needs
 from exts.cogs import globalvars
 #import urllib.request as ur
 
+from paginator import *
 
 class General(commands.Cog):
     '''General commands'''
@@ -276,6 +277,27 @@ class General(commands.Cog):
             u.remove("IU Bot")
             winner = random.choice(u)
             await msg.edit(content = '{} has won the giveaway!'.format(winner), embed=None)
+    
+    @commands.command(name='help')
+    async def _help(self, ctx, *, command: str = None):
+        """Shows help about a command or the bot"""
+        try:
+            if command is None:
+                p = await HelpPaginator.from_bot(ctx)
+            else:
+                entity = bot.get_cog(command) or bot.get_command(command)
+
+                if entity is None:
+                    clean = command.replace('@', '@\u200b')
+                    return await ctx.send(f'Command or category "{clean}" not found.')
+                elif isinstance(entity, commands.Command):
+                    p = await HelpPaginator.from_command(ctx, entity)
+                else:
+                    p = await HelpPaginator.from_cog(ctx, entity)
+
+            await p.paginate()
+        except Exception as e:
+            await ctx.send(traceback.format_exception(None, e, e.__traceback__))
 
 def setup(bot):
     bot.add_cog(General(bot))
