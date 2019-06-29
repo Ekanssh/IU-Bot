9476a48896a0f3104a5015b5b825d5c252a410d7
Ekansh kashyap <cityexevision@gmail.com>
2019-06-29 19:26:03 +0530

    protecc fix

--- exts/cogs/Events.py
+++ exts/cogs/Events.py
@@ -81,7 +81,7 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
         if reaction.message.channel.id != 567050071628054532 and reaction.message.guild.id == 281793428793196544:
-            if ((reaction.emoji == "\u2b50") or (reaction.emoji == "🌟")) and reaction.count > 3:
+            if ((reaction.emoji == "\u2b50") or (reaction.emoji == "🌟")) and reaction.count > 2:
                 message = reaction.message
 
                 m = await discord.utils.get(self.bot.guilds[0]. channels, name="starboard").history(limit=20).flatten()
